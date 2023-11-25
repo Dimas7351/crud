0f1d3e981d4ea298d72326d32d4bed0223436a90
Dimas735 <dmamugin@gmail.com>
2023-11-26 01:15:54 +0300

    lesson 21 DAO

---
+++ src/main/java/org/example/controllers/PeopleController.java
@@ -0,0 +1,36 @@
+package org.example.controllers;
+
+
+import org.example.dao.PersonDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PersonDAO personDAO;
+
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model){
+        //Получим всех людей из DAO и передадим в view
+        model.addAttribute("people",personDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model){
+        // Получим одного человека по id из DAO и передадим в view
+        model.addAttribute("person",personDAO.show(id));
+        return "people/show";
+    }
+}
